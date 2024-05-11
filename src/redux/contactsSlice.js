8f6f455e43294927cfb4b547a45954e217e1c379
Ovoyana <olyakovbas@gmail.com>
2024-05-11 19:54:20 +0200

    update main

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -9,7 +9,7 @@ const slice = createSlice({
     reducers: {
         addContact: {
             reducer(state, action) {
-                state.push(action.payload)
+                state.items.push(action.payload)
             },
             prepare(values) {
                 return {
@@ -22,7 +22,7 @@ const slice = createSlice({
         },
         
         deleteContact: (state, action) => {
-            return state.filter(contact => contact.id !== action.payload);
+            return state.items.filter(contact => contact.id !== action.payload);
         }
     }
     })
